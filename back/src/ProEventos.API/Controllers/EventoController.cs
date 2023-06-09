d3df239cd4967e39ebfc4badbef1ffe694450010
anderson <andersonairesspp@hotmail.com>
2023-06-09 11:37:31 -0300

    section 2: .NET

---
+++ back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _evento = new Evento[]{
+            new Evento(){
+                    EventoId = 1,
+                    Tema = "Angular 11 e .NET 5",
+                    local = "Belo Horizonte",
+                    Lote = "1º Lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString(),
+                    ImagemURL = "foto.png"
+                },
+                new Evento(){
+                    EventoId = 2,
+                    Tema = "React e SpringBoot",
+                    local = "Natal",
+                    Lote = "1º Lote",
+                    QtdPessoas = 200,
+                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
+                    ImagemURL = "image.png"
+                },
+        };
+        
+        public EventoController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> Get(int id){
+            return _evento.Where(Evento => Evento.EventoId ==id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "exemplo ded Post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return "exemplo de Put com id = {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return "exemplo de Delete com id = {id}";
+        }
+    }
+}
