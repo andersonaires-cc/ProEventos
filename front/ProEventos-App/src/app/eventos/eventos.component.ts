b960f7f8866933ed323b2f5e1bede4189160b6c6
anderson <andersonairesspp@hotmail.com>
2023-06-14 21:36:29 -0300

    ngFor

--- front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./eventos.component.scss']
 })
 export class EventosComponent {
-
+  public eventos: any = [
+    {
+      Tema : 'Angular 15',
+      Local: 'Belo Horizonte'
+    },
+    {
+      Tema : '.NET 5',
+      Local: 'São Paulo'
+    },
+    {
+      Tema : 'Angular e suas novidades',
+      Local: 'Rio de Janeiro'
+    }
+  ]
 }
