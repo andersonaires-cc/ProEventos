7be0645d9f9ca4f1a0f52f799558de80e6cd7db4
anderson <andersonairesspp@hotmail.com>
2023-06-14 22:12:30 -0300

    Config CORS

--- front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,18 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./eventos.component.scss']
 })
 export class EventosComponent {
-  public eventos: any = [
-    {
-      Tema : 'Angular 15',
-      Local: 'Belo Horizonte'
-    },
-    {
-      Tema : '.NET 5',
-      Local: 'São Paulo'
-    },
-    {
-      Tema : 'Angular e suas novidades',
-      Local: 'Rio de Janeiro'
-    }
-  ]
+  public eventos: any;
+
+  constructor(private http:HttpClient){}
+
+  ngOnInit(): void{
+    this.getEventos();
+  }
+  public getEventos(): void{
+    this.http.get('https://localhost:5001/api/eventos').subscribe(
+      response => this.eventos = response,
+      error => console.log(error)
+    );
+
+  }
 }
