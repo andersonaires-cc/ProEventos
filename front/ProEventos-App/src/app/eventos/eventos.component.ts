62850b3978018826477c80c556cbb20b4498954c
anderson <andersonairesspp@hotmail.com>
2023-06-22 22:09:27 -0300

    Filter

--- front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,12 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./eventos.component.scss']
 })
 export class EventosComponent {
+  
   public eventos: any = [];
+  public eventosFiltrados: any = [];
+
   larguraImagem: number = 150;
   margemImagem: number = 2;
   exibirImagem: boolean = true;
-  filtroLista: string = '';
+  private _filtroLista: string = '';
 
+  public get filtroLista(){
+    return this._filtroLista;
+  }
+  public set filtroLista(value : string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this._filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+        (evento:
+            {local: any; tema: string;}) => 
+                evento.tema.toLocaleLowerCase().indexOf(filtrarPor)!==-1
+                ||
+                evento.local.toLocaleLowerCase().indexOf(filtrarPor)!==-1
+    )
+  }
   
   constructor(private http:HttpClient){}
 
@@ -26,7 +47,10 @@ export class EventosComponent {
 
   public getEventos(): void{
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+    },
       error => console.log(error)
     );
 
