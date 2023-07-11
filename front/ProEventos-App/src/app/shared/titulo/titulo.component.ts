84a40f8acedbe77b1615e7d915e626bf82d970b5
anderson <andersonairesspp@hotmail.com>
2023-07-11 19:56:50 -0300

    @Input

--- front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
+++ front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-titulo',
   templateUrl: './titulo.component.html',
   styleUrls: ['./titulo.component.scss']
 })
-export class TituloComponent {
+export class TituloComponent  implements OnInit{
+    @Input()
+    titulo!: string;
 
+    constructor(){}
+
+    ngOnInit(): void {
+        
+    }
 }
