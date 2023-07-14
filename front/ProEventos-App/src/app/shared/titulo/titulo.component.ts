fece9aa6943a52f6d8580670e7f4d6c4b98f9958
anderson <andersonairesspp@hotmail.com>
2023-07-14 13:29:49 -0300

    titulo

--- front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
+++ front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
@@ -9,6 +9,11 @@ export class TituloComponent  implements OnInit{
     @Input()
     titulo!: string;
 
+    @Input() Titulo: string | undefined;
+    @Input() iconClass = 'fa fa-user';
+    @Input() subtitulo = 'Desde 2023';
+    @Input() botaoListar = false;
+
     constructor(){}
 
     ngOnInit(): void {
