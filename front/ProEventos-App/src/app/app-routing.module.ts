1e625ed72504e77b8f7a3f19ef5d96e169662870
anderson <andersonairesspp@hotmail.com>
2023-07-16 11:36:59 -0300

    subroutes

--- front/ProEventos-App/src/app/app-routing.module.ts
+++ front/ProEventos-App/src/app/app-routing.module.ts
@@ -6,9 +6,18 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
 import { ContatosComponent } from './components/contatos/contatos.component';
 import { PerfilComponent } from './components/perfil/perfil.component';
+import { EventoDetalheComponent } from './components/eventos/evento-detalhe/evento-detalhe.component';
+import { EventoListaComponent } from './components/eventos/evento-lista/evento-lista.component';
 
 const routes: Routes = [
-    {path: 'eventos',component: EventosComponent},
+    {
+        path: 'eventos',component: EventosComponent,
+        children : [
+            {path: 'detalhe/:id',component: EventoDetalheComponent},
+            {path: 'detalhe',component: EventoDetalheComponent},
+            {path: 'lista',component: EventoListaComponent},
+        ]
+    },
     {path: 'dashboard',component: DashboardComponent},
     {path: 'palestrantes',component: PalestrantesComponent},
     {path: 'contatos',component: ContatosComponent},
--- front/ProEventos-App/src/app/components/eventos/eventos.component.ts
+++ front/ProEventos-App/src/app/components/eventos/eventos.component.ts
@@ -13,76 +13,7 @@ import { NgxSpinnerService } from 'ngx-spinner';
 //   providers : [EventoService]
 })
 export class EventosComponent {
-  
-  modalRef?: BsModalRef;
-  public eventos: Evento[] = [];
-  public eventosFiltrados: Evento[] = [];
+  ngOnInit(): void{
 
-  public larguraImagem = 150;
-  public margemImagem = 2;
-  public exibirImagem = true;
-  private filtroListado = '';
-
-  public get filtroLista(){
-    return this.filtroListado;
   }
-  public set filtroLista(value : string){
-    this.filtroListado = value;
-    this.eventosFiltrados = this.filtroListado ? this.filtrarEventos(this.filtroLista) : this.eventos;
-  }
-
-  public filtrarEventos(filtrarPor: string): Evento[]{
-    filtrarPor = filtrarPor.toLocaleLowerCase();
-    return this.eventos.filter(
-        (evento:
-            {local: any; tema: string;}) => 
-                evento.tema.toLocaleLowerCase().indexOf(filtrarPor)!==-1
-                ||
-                evento.local.toLocaleLowerCase().indexOf(filtrarPor)!==-1
-    )
-  }
-  
-  constructor(
-    private eventoService: EventoService,
-    private modalService: BsModalService,
-    private toastr: ToastrService,
-    private spinner: NgxSpinnerService
-  ){}
-
-  public ngOnInit(): void{
-    this.getEventos();
-    this.spinner.show();
-  }
-
-  public alterarImagem(): void{
-    this.exibirImagem = !this.exibirImagem;
-  }
-
-  public getEventos(): void{
-    this.eventoService.getEventos().subscribe({
-      next: (eventos: Evento[]) => {
-        this.eventos = eventos;
-        this.eventosFiltrados = this.eventos;
-      },
-      error: (error: any) => {
-        this.spinner.hide();
-        this.toastr.error('Erro ao carregar os eventos','Error!')
-      },
-      complete : () => this.spinner.hide()
-    });
-  }
-
-  openModal(template: TemplateRef<any>): void {
-    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
-  }
- 
-  confirm(): void {
-    this.modalRef?.hide();
-    this.toastr.success('O Evento foi deletado com sucesso', 'Deletado!');
-  }
- 
-  decline(): void {
-    this.modalRef?.hide();
-  }
-
 }
