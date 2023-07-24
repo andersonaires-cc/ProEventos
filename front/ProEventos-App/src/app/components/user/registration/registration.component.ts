a5a01098059147a2dfccd20ecdb2843d513383de
anderson <andersonairesspp@hotmail.com>
2023-07-24 08:05:39 -0300

    Senha

--- front/ProEventos-App/src/app/components/user/registration/registration.component.ts
+++ front/ProEventos-App/src/app/components/user/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from '@app/helpers/validator-field';
 
 @Component({
   selector: 'app-registration',
@@ -18,6 +19,11 @@ export class RegistrationComponent implements OnInit{
         this.validation();
     }
     private validation(): void {
+
+        const formOptions: AbstractControlOptions ={
+            validators: ValidatorField.MustMatch('senha','confirmeSenha')
+        };
+
         this.form = this.fb.group({
             primeiroNome:['', Validators.required] ,
             ultimoNome:['', Validators.required] ,
@@ -29,6 +35,6 @@ export class RegistrationComponent implements OnInit{
                 [Validators.required, Validators.minLength(6)]
             ],
             confirmeSenha:['', Validators.required] ,
-        });
+        }, formOptions);
     }
 }
