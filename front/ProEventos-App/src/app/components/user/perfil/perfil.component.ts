a01e0649566c42bef2995626d30fec484344c1e3
anderson <andersonairesspp@hotmail.com>
2023-07-24 09:13:06 -0300

    Form Perfil incompleted

--- front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
+++ front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.scss']
 })
-export class PerfilComponent {
+export class PerfilComponent implements OnInit {
 
+    form!: FormGroup;
+
+    constructor(public fb: FormBuilder){}
+
+    get f(): any {return this.form.controls;}
+
+    ngOnInit(): void {
+        this.validation();
+    }
+
+    private validation(): void{
+
+
+        this.form = this.fb.group({
+            titulo :['', Validators.required] ,
+            primeiroNome:['', Validators.required] ,
+            ultimoNome: ['', Validators.required],
+            email:['', Validators.required] ,
+            telefone:['', Validators.required] ,
+            funcao:['', Validators.required] ,
+            senha:['', Validators.required] ,
+            confirmeSenha:['', Validators.required] ,
+        })
+    }
 }
