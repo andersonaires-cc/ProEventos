d195bf3dceec249c789258d4b97ace610c974792
anderson <andersonairesspp@hotmail.com>
2023-07-23 22:17:32 -0300

    Form Registration 01

--- front/ProEventos-App/src/app/components/user/registration/registration.component.ts
+++ front/ProEventos-App/src/app/components/user/registration/registration.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss']
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit{
+    
+    form!: FormGroup;
 
+    constructor(public fb: FormBuilder){}
+
+    ngOnInit(): void {
+        this.validation();
+    }
+    private validation(): void {
+        this.form = this.fb.group({
+            primeiroNome:['', Validators.required] ,
+            ultimoNome:['', Validators.required] ,
+            email:['', 
+                [Validators.required, Validators.email]
+            ],
+            userName:['', Validators.required] ,
+            senha:['', 
+                [Validators.required, Validators.minLength(6)]
+            ],
+            confirmeSenha:['', Validators.required] ,
+        });
+    }
 }
