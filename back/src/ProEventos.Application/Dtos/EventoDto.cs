98914b088c568a73de6d94d5ac9a9fbddf642f13
anderson <andersonairesspp@hotmail.com>
2023-08-20 09:44:45 -0300

    Data annotations

--- back/src/ProEventos.Application/Dtos/EventoDto.cs
+++ back/src/ProEventos.Application/Dtos/EventoDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,10 +11,14 @@ namespace ProEventos.Application.Dtos
         public int Id { get; set; }
         public string Local { get; set; }
         public string DataEvento { get; set; }
+
+        [Required(ErrorMessage ="O campo {0} é obrigatório")]
         public string Tema { get; set; }
         public int QtdPessoas { get; set; }
         public string ImagemURL { get; set; }
         public string Telefone { get; set; }
+
+        [EmailAddress(ErrorMessage ="O campo {0} precisa ser um email válido")]
         public string Email { get; set; }
         public IEnumerable<LoteDto> Lotes { get; set; }
         public IEnumerable<RedeSocialDto> RedesSociais { get; set; }
