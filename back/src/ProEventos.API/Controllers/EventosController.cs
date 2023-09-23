acc3190e2c3ed87477de04aa9f9434ca0b933027
anderson <andersonairesspp@hotmail.com>
2023-09-23 10:25:00 -0300

    Method Save file

--- back/src/ProEventos.API/Controllers/EventosController.cs
+++ back/src/ProEventos.API/Controllers/EventosController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using ProEventos.Application.Dtos;
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using System.Linq;
 
 namespace ProEventos.API.Controllers
 {
@@ -87,7 +88,7 @@ namespace ProEventos.API.Controllers
                 var file = Request.Form.Files[0];
                 if(file.Length >0){
                     DeleteImage(evento.ImagemURL);
-                    //evento.ImagemURL = SaveIamge(file);
+                    evento.ImagemURL = await SaveImage(file);
                 }
 
                 var EventoRetorno = await _eventoService.UpdateEvento(eventoId,evento);
@@ -155,6 +156,22 @@ namespace ProEventos.API.Controllers
                         $"Erro ao tentar deletar eventos. Erro: {ex.Message}");
             }
         }
+
+        [NonAction]
+        public async Task<string> SaveImage(IFormFile imageFile){
+            string imageName = new String(Path.GetFileNameWithoutExtension(imageFile.FileName)
+                                .Take(10)
+                                .ToArray()).Replace(' ', '-');
+
+            imageName = $"{imageName}{DateTime.UtcNow.ToString("yymmssfff")}{Path.GetExtension(imageFile.FileName)}";
+
+            var imagePath = Path.Combine(_hostEnvironment.ContentRootPath, @"Resources/Images", imageName);
+
+            using (var fileStream = new FileStream(imagePath, FileMode.Create)){
+                await imageFile.CopyToAsync(fileStream);
+            }
+            return imageName;
+        }        
 
         [NonAction]
         public void DeleteImage(string imageName){
