0307499c339f5709b6edc893b4f91290b73082e8
anderson <andersonairesspp@hotmail.com>
2023-10-03 14:31:16 -0300

    Create tables identity

--- back/src/ProEventos.Domain/Identity/User.cs
+++ back/src/ProEventos.Domain/Identity/User.cs
@@ -8,7 +8,7 @@ namespace ProEventos.Domain.Identity
     {
         public string PrimeiroNome {get;set;}
         public string UltimoNome {get;set;}
-        public Titulo Titulo {get;set}
+        public Titulo Titulo {get;set;}
         public string Descricao {get;set;}
         public Funcao Funcao {get;set;}
         public string ImagemURL {get;set;}
