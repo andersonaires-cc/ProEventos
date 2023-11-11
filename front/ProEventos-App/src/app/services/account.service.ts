7b48dec1aeb5b0a6e13dac60771044cf0a123bc1
anderson <andersonairesspp@hotmail.com>
2023-11-10 21:33:50 -0300

    Service Account

---
+++ front/ProEventos-App/src/app/services/account.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '@app/models/identity/User';
+import { environment } from '@environments/environment';
+import { Observable } from 'rxjs';
+import {map, take } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+
+  baseUrl = environment.apiURL = 'api/account/'  
+  constructor(private http: HttpClient) { }
+
+  public login(model: any): Observable<void>{
+    return this.http.post<User>(this.baseUrl + 'login', model).pipe(
+        take(1),
+        map((response: User) =>{
+            const user = response;
+            if(user){
+
+            }
+        })
+    );
+  }
+}
